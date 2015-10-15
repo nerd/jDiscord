249b2935fdfe85a65cbc76b05043bbbfe63bfc16
Ghost <ghost@ghosted.me>
2015-10-15 23:39:06 +0100

    Update Login.java

--- src/main/java/me/itsghost/jdiscord/internal/Login.java
+++ src/main/java/me/itsghost/jdiscord/internal/Login.java
@@ -20,40 +20,58 @@ public class Login {
     private String token;
     public void process(DiscordAPI api) throws BadUsernamePasswordException, DiscordFailedToConnectException {
         api.log("Attempting to login!");
-        PacketBuilder pb = new PacketBuilder(api);
-        pb.setSendLoginHeaders(false);
-        pb.setData(new JSONObject().put("email", username).put("password", password).toString());
-        pb.setUrl("https://discordapp.com/api/auth/login");
-        pb.setType(RequestType.POST);
-        final String out = pb.makeRequest();
-        if (out == null)
-            throw new DiscordFailedToConnectException();
-        JSONObject outJson = new JSONObject(out);
+
+        JSONObject outJson = getTokens(api);
+
         if (outJson.isNull("token"))
             throw new BadUsernamePasswordException();
+        
         token = outJson.getString("token");
+
         api.log("Logged in and starting session!");
         api.log("Token: " + token);
+
         api.setRequestManager(new RequestManager(api));
         WebSocketClient socket = api.getRequestManager().getSocketClient();
-        JSONObject login = new JSONObject();
-        login.put("op", 2)
-                .put("d", new JSONObject()
-                        .put("token", token)
-                        .put("properties", new JSONObject()
-                            .put("$os", "Windows")
-                            .put("$browser", "jDiscord by GitHub user NotGGhost")
-                            .put("$device", "")
-                            .put("$referring_domain", "t.co")
-                            .put("$referrer", "")
-                        )
-                        .put("v", 3)
-                );
+
         while (!socket.loaded){
             try{
                 Thread.sleep(200);
             }catch(Exception e){}
         }
-        socket.send(login.toString());
+
+        socket.send(getPCMetadata());
+    }
+    
+    public JSONObject getTokens(DiscordAPI api) throws DiscordFailedToConnectException{
+        PacketBuilder pb = new PacketBuilder(api);
+        pb.setSendLoginHeaders(false);
+        pb.setData(new JSONObject().put("email", username).put("password", password).toString());
+        pb.setUrl("https://discordapp.com/api/auth/login");
+        pb.setType(RequestType.POST);
+        
+        final String out = pb.makeRequest();
+
+        if (out == null)
+            throw new DiscordFailedToConnectException();
+
+        return new JSONObject(out);
+    }
+    
+    public String getPCMetadata(){
+        JSONObject login = new JSONObject();
+        login.put("op", 2)
+                .put("d", new JSONObject()
+                        .put("token", token)
+                        .put("properties", new JSONObject()
+                                .put("$os", "Windows")
+                                .put("$browser", "jDiscord by GitHub user NotGGhost")
+                                .put("$device", "")
+                                .put("$referring_domain", "t.co")
+                                .put("$referrer", "")
+                        )
+                        .put("v", 3)
+                );
+        return login.toString();
     }
 }
